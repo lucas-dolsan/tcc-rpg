321f7cbb757af87f8a3709116cd19cc5f362c90e
Lucas Dolsan <lucas.dolsan1775@gmail.com>
2017-04-17 16:43:59 -0300

    Optimizações na TelaInicial, TelaJogo, e TelaConfigurarSala.

---
+++ TCC/src/Telas/TelaInicial.java
@@ -0,0 +1,154 @@
+package Telas;
+
+import ConexaoBanco.JogadorDAO;
+import java.awt.event.KeyEvent;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
+public class TelaInicial extends javax.swing.JFrame {
+
+    public TelaInicial() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        botaoCriarSala = new javax.swing.JButton();
+        botaoEntrarEmSala = new javax.swing.JButton();
+        botaoDeslogar = new javax.swing.JButton();
+        planoDeFundoTelaInicial = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("RPG - TCC - " + JogadorDAO.nickName);
+        setMaximumSize(new java.awt.Dimension(1280, 720));
+        setMinimumSize(new java.awt.Dimension(1280, 720));
+        setPreferredSize(new java.awt.Dimension(1280, 720));
+        setResizable(false);
+        getContentPane().setLayout(null);
+
+        jLabel1.setFont(new java.awt.Font("The Bold Font", 1, 48)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("SEJA BEM-VINDO");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(425, 30, 440, 110);
+
+        jLabel2.setFont(new java.awt.Font("The Bold Font", 1, 48)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText(JogadorDAO.nickName);
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(350, 110, 580, 80);
+
+        botaoCriarSala.setFont(new java.awt.Font("The Bold Font", 1, 24)); // NOI18N
+        botaoCriarSala.setText("CRIAR NOVA SALA");
+        botaoCriarSala.setName(""); // NOI18N
+        botaoCriarSala.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoCriarSalaActionPerformed(evt);
+            }
+        });
+        botaoCriarSala.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                botaoCriarSalaKeyPressed(evt);
+            }
+        });
+        getContentPane().add(botaoCriarSala);
+        botaoCriarSala.setBounds(430, 240, 450, 60);
+
+        botaoEntrarEmSala.setFont(new java.awt.Font("The Bold Font", 1, 24)); // NOI18N
+        botaoEntrarEmSala.setText("ENTRAR EM UMA SALA");
+        botaoEntrarEmSala.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                botaoEntrarEmSalaKeyPressed(evt);
+            }
+        });
+        getContentPane().add(botaoEntrarEmSala);
+        botaoEntrarEmSala.setBounds(430, 330, 450, 60);
+
+        botaoDeslogar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icones/deslogar.png"))); // NOI18N
+        botaoDeslogar.setToolTipText("Desconectar.");
+        botaoDeslogar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoDeslogarActionPerformed(evt);
+            }
+        });
+        botaoDeslogar.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                botaoDeslogarKeyPressed(evt);
+            }
+        });
+        getContentPane().add(botaoDeslogar);
+        botaoDeslogar.setBounds(1190, 650, 80, 40);
+
+        planoDeFundoTelaInicial.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icones/telaLogin.jpg"))); // NOI18N
+        planoDeFundoTelaInicial.setAlignmentY(0.0F);
+        planoDeFundoTelaInicial.setMaximumSize(new java.awt.Dimension(1280, 720));
+        planoDeFundoTelaInicial.setMinimumSize(new java.awt.Dimension(1280, 720));
+        planoDeFundoTelaInicial.setPreferredSize(new java.awt.Dimension(1280, 720));
+        getContentPane().add(planoDeFundoTelaInicial);
+        planoDeFundoTelaInicial.setBounds(0, 0, 1280, 720);
+        planoDeFundoTelaInicial.getAccessibleContext().setAccessibleDescription("");
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+private void deslogar() {
+        int sair = JOptionPane.showConfirmDialog(null, "Deseja sair?", "Desconectar.", JOptionPane.YES_NO_OPTION);
+        if (sair == JOptionPane.YES_OPTION) {
+            this.dispose();
+            TelaLogin.abrirTela();
+        }
+    }
+    private void botaoDeslogarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoDeslogarActionPerformed
+        deslogar();
+    }//GEN-LAST:event_botaoDeslogarActionPerformed
+
+    private void botaoCriarSalaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoCriarSalaActionPerformed
+        JDialog tela = new TelaConfigurarSala(this, true);
+        tela.setVisible(true);
+    }//GEN-LAST:event_botaoCriarSalaActionPerformed
+
+    private void botaoCriarSalaKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_botaoCriarSalaKeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+            JDialog tela = new TelaConfigurarSala(this, true);
+            tela.setVisible(true);
+        } else if (evt.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            deslogar();
+        }
+    }//GEN-LAST:event_botaoCriarSalaKeyPressed
+
+    private void botaoEntrarEmSalaKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_botaoEntrarEmSalaKeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            deslogar();
+        }
+    }//GEN-LAST:event_botaoEntrarEmSalaKeyPressed
+
+    private void botaoDeslogarKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_botaoDeslogarKeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            deslogar();
+        }
+    }//GEN-LAST:event_botaoDeslogarKeyPressed
+
+    public static void Start() {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaInicial().setVisible(true);
+            }
+        }
+        );
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botaoCriarSala;
+    private javax.swing.JButton botaoDeslogar;
+    private javax.swing.JButton botaoEntrarEmSala;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel planoDeFundoTelaInicial;
+    // End of variables declaration//GEN-END:variables
+}
