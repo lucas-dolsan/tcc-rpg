14ca800c91cb69493d995ed5476d2474f2be210a
Lucas Dolsan <lucas.dolsan1775@gmail.com>
2017-06-13 18:27:54 -0300

    Informações do banco protegidas em arquivo externo não-versionável

--- TCC/src/ConexaoBanco/ConexaoMySql.java
+++ TCC/src/ConexaoBanco/ConexaoMySql.java
@@ -1,7 +1,9 @@
-
 package ConexaoBanco;
 
 import com.mysql.jdbc.Connection;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.*;
 
@@ -12,10 +14,30 @@ public final class ConexaoMySql {
     public static ConexaoMySql connection;
 
     private ConexaoMySql() {
-        String usuario = "schork_dolsan";
-        String senha = "dh4_fT2kl@d";
-        String driver = "com.mysql.jdbc.Driver";
-        String enderecoBanco = "schork_dolsan.mysql.dbaas.com.br:3306/schork_dolsan";
+
+        String[] linhas = new String[4];
+        try {
+
+            FileReader reader = new FileReader("C:\\TCC-RPG\\TCC-RPG-DB.txt");
+            BufferedReader bf = new BufferedReader(reader);
+            String line;
+            int i = 0;
+            while ((line = bf.readLine()) != null) {
+
+                linhas[i] = line;
+                i++;
+
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String usuario = linhas[0];
+        String senha = linhas[1];
+        String driver = linhas[2];
+        String enderecoBanco = linhas[3];
         try {
             Class.forName(driver).newInstance();
             this.c = (Connection) DriverManager.getConnection("jdbc:mysql://" + enderecoBanco, usuario, senha);
